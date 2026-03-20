e180470efff699cc4772edf0c7f60405b3ef604b
azusa1120 <azunanashiee@gmail.com>
2026-03-20 22:11:23 +0900

    #1 プロフィール編集画面作成

---
+++ app/src/main/java/com/example/profile/ui/screen/EditScreen.kt
@@ -0,0 +1,18 @@
+package com.example.profile.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.profile.ui.theme.ProfileTheme
+
+@Composable
+fun EditScreen() {
+    ProfileTheme {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EditScreenPreview() {
+    EditScreen()
+}
