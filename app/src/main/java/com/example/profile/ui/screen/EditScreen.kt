d91c1468a074e6e81e3748f0a48619c4caf39e53
azusa1120 <azunanashiee@gmail.com>
2026-03-24 16:11:53 +0900

    #3 トグルボタン作成

--- app/src/main/java/com/example/profile/ui/screen/EditScreen.kt
+++ app/src/main/java/com/example/profile/ui/screen/EditScreen.kt
@@ -1,13 +1,21 @@
 package com.example.profile.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Public
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -16,10 +24,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.profile.R
 import com.example.profile.ui.theme.ProfileTheme
 
 @Composable
@@ -35,6 +46,12 @@ fun EditScreen() {
                     Form("氏名", "山田 太郎", KeyboardType.Text)
                     Form("メールアドレス", "sample@example", KeyboardType.Email)
                     Form("自己紹介", "趣味や仕事を書きましょう", KeyboardType.Text)
+                }
+            }
+            Part("設定") {
+                Column {
+                    Toggle("プッシュ通知", Icons.Default.Notifications)
+                    Toggle("公開プロフィール", Icons.Default.Public)
                 }
             }
         }
@@ -58,6 +75,36 @@ fun Form(title: String, hint: String, type: KeyboardType) {
                 focusedIndicatorColor = Color.Transparent,
                 unfocusedIndicatorColor = Color.Transparent
             )
+        )
+    }
+}
+
+@Composable
+fun Toggle(title: String, icon: ImageVector) {
+    val checked = remember{ mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth().padding(10.dp, 5.dp)
+            .background(color = Color(0xFFE2E2ED),  shape = RoundedCornerShape(4.dp)),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier.padding(10.dp, 0.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = stringResource(id = R.string.toggle_icon_description),
+                modifier = Modifier.padding(0.dp, 16.dp, 10.dp, 16.dp),
+                tint = Color(0xFF4F629F)
+            )
+            Text(text = title)
+        }
+        Switch(
+            checked = checked.value,
+            onCheckedChange = { checked.value = it },
+            modifier = Modifier.padding(0.dp, 0.dp, 10.dp, 0.dp)
         )
     }
 }
